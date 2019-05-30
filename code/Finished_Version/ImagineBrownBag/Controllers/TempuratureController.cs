c8656675c4c944949dd130c520e256561529506d
Pangamma <planetman.hack@gmail.com>
2019-05-30 00:29:38 -0700

    initial commit
    initial commit

---
+++ code/Finished_Version/ImagineBrownBag/Controllers/TempuratureController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImagineBrownBag.Controllers
+{
+    [HasPermission(Permissions.CanViewTempurature)]
+    public class TempuratureController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            //Fetch temp and display it
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [HasPermission(Permissions.CanAdjustTempurature, Permissions.CanViewTempurature)]
+        [HasPermission(Permissions.CanPlayVideoGames)]
+        public ActionResult SetTempurature(int newTempurature)
+        {
+            // Set tempurature 
+            return View();
+        }
+    }
+}
+
+
+
---
+++ code/Finished_Version/ImagineBrownBag/Global.asax.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace ImagineBrownBag
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            Permissions.GetRolesCsvForCurrentUser = () =>
+            {
+                string rolesCsv = "";
+                // Don't actually use this custom cookie auth method in a production system. This is not secure.
+                if (HttpContext.Current.Request.QueryString.AllKeys.Any(x => x == "Roles"))
+                {
+                    string[] roles = HttpContext.Current.Request.QueryString.GetValues("Roles");
+                    rolesCsv = string.Join(",", string.Join(",", roles).Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries));
+                    HttpContext.Current.Response.SetCookie(new HttpCookie("Roles", rolesCsv)
+                    {
+                        HttpOnly = true,
+                        Expires = DateTime.MaxValue,
+                    });
+                }
+
+                if (HttpContext.Current.Request.Cookies.AllKeys.Any(x => x == "Roles"))
+                {
+                    HttpCookie cookie = HttpContext.Current.Request.Cookies.Get("Roles");
+                    rolesCsv = cookie.Value;
+                }
+
+                return rolesCsv;
+            };
+        }
+    }
+}
